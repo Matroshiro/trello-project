702765c1e3cf7671218b61634e672b1393689034
Tristan Goubeau <tristan.goubeau@epitech.eu>
2019-11-12 18:50:49 +0100

    added some css

--- App.jsx
+++ App.jsx
@@ -7,56 +7,85 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            dataList: [{
+            dataLists: [{
                 id: 1,
                 name: "To do"
             },
             {
-                id:2,
+                id: 2,
                 name: "Doing"
             },
             {
-                id:3,
+                id: 3,
                 name: "Done"
+            },
+            {
+                id: 4,
+                name: "Test"
             }
-        ],
-        dataCards : [{
-            id: 2   ,
-            cardTitle: "Push le projet",
-            idList : 1,
-        },
-        {
-            id: 2,
-            cardTitle: "Faire le trello",
-            idList : 2,
-        },
-        {
-            id: 3,
-            cardTitle: "Styliser le trello",
-            idList : 2,
-        },
-        {
-            id: 4,
-            cardTitle: "Setup l'environnement",
-            idList : 3,
-        }
-        ]
+            ],
+            dataCards: [{
+                id: 1,
+                cardTitle: "Push le projet",
+                idList: 1,
+            },
+            {
+                id: 2,
+                cardTitle: "Faire le trello",
+                idList: 2,
+            },
+            {
+                id: 3,
+                cardTitle: "Styliser le trello",
+                idList: 2,
+            },
+            {
+                id: 4,
+                cardTitle: "Setup l'environnement",
+                idList: 3,
+            },
+            {
+                id: 5,
+                cardTitle: "Setup l'environnement",
+                idList: 4,
+            }
+            ]
         }
     }
 
     render() {
         return (
             <div>
-                <h1>My homemade trello</h1>
-                <div className="card" style={{width: "18rem"}}>
-                    <img src="https://www.sciencesetavenir.fr/assets/img/2016/09/02/cover-r4x3w1000-5834b11017175-elon-musk.jpg" className="card-img-top" alt="..."></img>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
+                <h1 className="text-center p-4 main-title">My homemade trello</h1>
+                <div className="card-deck container-fluid row">
+                    {this.state.dataLists.map((list, index) => {
+                        const cards = this.state.dataCards
+                            .filter(card => card.idList == list.id);
+                        return (
+                            <div className="col-lg-2 list-content" key={index}>
+                                <div className="card-title task-title text-center p-3">{list.name}</div>
+                                <div className="card">
+                                    {
+                                        cards.map((card, index) => {
+                                            console.log("card = " + JSON.stringify(card));
+                                            return (
+                                                <div className="card-body" key={index}>
+                                                    <div className="card-title">{card.cardTitle}</div>
+                                                    <div className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</div>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                    <div className="card-footer">
+                                        <small className="text-muted">Last updated 3 mins ago</small>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                    )}
                 </div>
             </div>);
-        }
     }
+}
 export default App;
