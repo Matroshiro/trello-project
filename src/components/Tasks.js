1259cb03401f18ee245c8993a1c71a46ec07e775
Tristan Goubeau <tristan.goubeau@epitech.eu>
2019-11-16 20:36:16 +0100

    task per list working

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -9,7 +9,8 @@ class Tasks extends React.Component {
         this.state = {
             dataLists: [],
             dataCards: [],
-            listName: ""
+            listName: "",
+            cardName: ""
         }
     }
 
@@ -27,20 +28,18 @@ class Tasks extends React.Component {
                     const lists = Object.keys(snapshot.val()).map(key => {
                         return {
                             key: key,
-                            listName : myListFromDatabase[key].listName
+                            listName: myListFromDatabase[key].listName
                         }
                     })
-                            console.log("datalist = " + JSON.stringify(lists));
-
                     this.setState({
-                        dataLists : lists
+                        dataLists: lists
                     })
                 }
             })
 
-            const myCard = firebase.database().ref('cards/')
+        const myCard = firebase.database().ref('cards/')
 
-            myCard
+        myCard
             .on('value', (snapshot) => {
                 const myCardFromDataBase = snapshot.val()
                 if (myCardFromDataBase === null) {
@@ -48,8 +47,9 @@ class Tasks extends React.Component {
                 } else {
                     const cards = Object.keys(snapshot.val()).map(key => {
                         return {
-                            key : key,
-                            cardName : myCardFromDataBase[key].cardName
+                            key: key,
+                            cardName: myCardFromDataBase[key].cardName,
+                            listKey: myCardFromDataBase[key].listKey
                         }
                     })
                     this.setState({
@@ -61,38 +61,90 @@ class Tasks extends React.Component {
 
     _handleChange = (e) => {
         this.setState({
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
-    _saveList  = (e) => {
-        console.log("passed")
+    _saveList = (e) => {
+        console.log("saved list")
         if (this.state.listName === '') {
             alert("this can not be empty")
         } else {
             const newListKey = firebase.database().ref('lists/').push().key;
             firebase
-            .database()
-            .ref('lists/')
-            .update({
-                [newListKey] : {
-                    listName: this.state.listName
-                }
-            })
+                .database()
+                .ref('lists/')
+                .update({
+                    [newListKey]: {
+                        listName: this.state.listName
+                    }
+                })
             this.setState({
-                listName : ''
+                listName: ''
+            })
+        }
+    }
+
+    _saveCard = (key, title, index, e) => {
+        if (this.state.cardName === '') {
+            alert("this can not be empty")
+        } else {
+            const newCardKey = firebase.database().ref('cards/').push().key;
+            firebase
+                .database()
+                .ref('cards/')
+                .update({
+                    [newCardKey]: {
+                        listKey: key.key,
+                        cardName: title.title
+                    }
+                })
+            this.setState({
+                cardName: ''
             })
         }
     }
 
     _handleDeleteList = (key) => {
+
+        const { dataCards } = this.state;
+        console.log(dataCards)
+        let cardsNbr = 0;
+
+        for (let i = 0; i < dataCards.length; i++) {
+            if (key === dataCards[i].listKey) {
+                cardsNbr++;
+            }
+        }
+
+        console.log("cardsNbr = " + cardsNbr)
+        if (cardsNbr > 0) {
+            for (let i = 0; i < dataCards.length; i++) {
+                if (key === dataCards[i].listKey) {
+                    this._handleDeleteCard(dataCards[i].key)
+                }
+            }
+        }
+
         firebase
-        .database()
-        .ref(`lists/${key}`)
-        .remove()
-        const listlenght = this.state.dataLists.length
-        if (listlenght === 1) {
-            this.setState({dataLists: []})
+            .database()
+            .ref(`lists/${key}`)
+            .remove()
+        const listLenght = this.state.dataLists.length
+        if (listLenght === 1) {
+            this.setState({ dataLists: [] })
+        }
+
+    }
+
+    _handleDeleteCard = key => {
+        firebase
+            .database()
+            .ref(`cards/${key}`)
+            .remove()
+        const cardsLenght = this.state.dataCards.length
+        if (cardsLenght === 1 || cardsLenght) {
+            this.setState({ dataCards: [] })
         }
     }
 
@@ -102,32 +154,58 @@ class Tasks extends React.Component {
                 <h1 className="text-center p-4 main-title">My homemade trello</h1>
                 <div className="card-deck container-fluid row margin-left">
                     {this.state.dataLists.map((list, index) => {
+                        
                         const cards = this.state.dataCards
-                            .filter(card => card.idList == list.id);
+                            .filter(card => card.listKey == list.key);
                         return (
                             <div className="col-lg-2 list-content" key={index}>
                                 <div className="card-title task-title text-center p-3">
                                     {list.listName}
-                                    <i className="fa fa-window-close" onClick={() => {this._handleDeleteList(list.key)
-                                    }} style={{float: "right"}}></i>
+                                    <i className="fa fa-window-close" onClick={() => {
+                                        this._handleDeleteList(list.key)
+                                    }} style={{ float: "right" }}></i>
                                 </div>
                                 <div className="card">
                                     {
                                         cards.map((card, index) => {
                                             return (
                                                 <div className="card-body" key={index}>
-                                                    <div className="card-title">{card.cardTitle}</div>
+                                                    <div className="card-title">{card.cardName}<i className="fa fa-window-close" style={{ float: "right" }}></i></div>
                                                     <div className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</div>
+                                                {
+                                                    index >= 1 ? (
+                                                        <button>Left</button>
+                                                    ) : (<button disabled>Left</button>)
+                                                }
+                                                {
+                                                    index < this.state.dataLists.length - 1 ? (
+                                                        <button>Right</button>
+                                                    ) : (<button disabled>Right</button>)
+                                                }
+                                                
                                                 </div>
                                             )
                                         })
                                     }
                                     <div className="card-footer">
                                         <div className="row">
-                                            <input 
-                                            placeholder="Add a task"                                            
+                                            <input
+                                                placeholder="Add a task"
+                                                type="text"
+                                                name="cardName"
+                                                value={this.state.cardName}
+                                                onChange={this._handleChange}
                                             />
-                                            <button>Save</button>
+                                            <button onClick={(key, title, index, e) =>
+
+                                                this._saveCard(
+                                                    { key: list.key },
+                                                    { title: this.state.cardName },
+                                                    { index: list.index },
+                                                    { e }
+                                                )
+
+                                            }>Save</button>
                                         </div>
 
                                         <small className="text-muted">Last updated 3 mins ago</small>
@@ -142,11 +220,11 @@ class Tasks extends React.Component {
                             <div className="card-header">
                                 <div className="row">
                                     <input
-                                    placeholder="Add a List"
-                                    type="text"
-                                    name="listName"
-                                    value={this.state.listName}
-                                    onChange={this._handleChange}/>
+                                        placeholder="Add a List"
+                                        type="text"
+                                        name="listName"
+                                        value={this.state.listName}
+                                        onChange={this._handleChange} />
                                     <button
                                         onClick={() => this._saveList()}
                                     >Save</button>
@@ -159,4 +237,5 @@ class Tasks extends React.Component {
             </div>);
     }
 }
+
 export default Tasks;
